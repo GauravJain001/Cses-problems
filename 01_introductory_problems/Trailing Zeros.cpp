4f96fefae42a06f28631bdaf4d969a3ccf48d3c2
Gaurav Jain <gauravjain.0184@gmail.com>
2025-09-01 23:41:20 +0530

    done

---
+++ 01_introductory_problems/Trailing Zeros.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+typedef long long ll;
+using namespace std;
+
+//algo
+//how many 5 * even number that is the number of zeros
+// Every multiple of 5 contributes at least one 5.
+// Numbers like 25 = 5×5 contribute two 5’s → add an extra count (n/25).
+// Numbers like 125 = 5×5×5 contribute three 5’s → add another count (n/125).
+//We never double-count, because each step only counts the extra 5s contributed by higher powers.
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll n;
+    cin >> n;
+    long long zeros = 0;
+    for (long long p = 5; p <= n; p *= 5) {
+        zeros += n / p;
+    }
+    cout << zeros;
+
+
+    return 0;
+}
--- 01_introductory_problems/Two Sets.cpp
+++ 01_introductory_problems/Two Sets.cpp
@@ -13,11 +13,46 @@
 #include <numeric>
 typedef long long ll;
 using namespace std;
+//algo
+// check sum of first n+1 number 1 to n
+// if sum is even only then possible
+// if possible take two sets then add the max ones to one set till it's set is smaller than
+// equal to halfsum 
+// if i becomes bigger then requires number to reach halfsum for set 1 add it to set 2
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+    ll n;
+    cin >> n;
+    ll sum = ((n+1)*(n))/2;
+    if(sum%2 == 1){
+        cout << "NO";
+        return 0;
+    }
+    cout << "YES\n";
+    vector<ll>s1;
+    vector<ll>s2;
+    ll halfSum = sum/2;
+    ll curSum= 0;
+    for(int i=n;i>=1;i--){
+        if(curSum + i <= halfSum){
+            curSum += i;
+            s1.push_back(i);
+        }else{
+            s2.push_back(i);
+        }
+    }
+    cout << s1.size() << endl;
+    for(int i=0;i<s1.size();i++){
+        cout << s1[i] << " ";
+    }
+    cout << "\n";
+    cout << s2.size() << endl;
+    for(int i=0;i<s2.size();i++){
+        cout << s2[i] << " ";
+    }
 
     return 0;
 }
